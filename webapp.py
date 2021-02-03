46602693ffb3d5f13f29081b933f3fd6bf034bdc
Juan Sebastian Roa <65826708+jsroa15@users.noreply.github.com>
2021-02-03 09:05:42 -0500

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -36,58 +36,58 @@ def user_input_features():
     
     #Scaled feature: I have to use mean and std fit in the StandarScaler
     
-    cons_12m=st.sidebar.slider('cons_12m',0,1000,0)
+    cons_12m=st.sidebar.slider('cons_12m',0,100000,0)
     cons_12m=np.log1p(cons_12m)
     
-    cons_gas_12m=st.sidebar.slider('cons_gas_12m',0,1000,0)
+    cons_gas_12m=st.sidebar.slider('cons_gas_12m',0,70000,0)
     cons_gas_12m=np.log1p(cons_gas_12m)
     
-    cons_last_month=st.sidebar.slider('cons_last_month',0,1000,0)
+    cons_last_month=st.sidebar.slider('cons_last_month',0,10000,0)
     cons_last_month=np.log1p(cons_last_month)
     
-    forecast_cons_12m=st.sidebar.slider('forecast_cons_12m',0,1000,0)
+    forecast_cons_12m=st.sidebar.slider('forecast_cons_12m',0,10000,0)
     forecast_cons_12m=np.log1p(forecast_cons_12m)
     
-    forecast_cons_year=st.sidebar.slider('forecast_cons_year',0,1000,0)
+    forecast_cons_year=st.sidebar.slider('forecast_cons_year',0,5000,0)
     forecast_cons_year=np.log1p(forecast_cons_year)
     
-    forecast_discount_energy=st.sidebar.slider('forecast_discount_energy',0,1000,0)
+    forecast_discount_energy=st.sidebar.slider('forecast_discount_energy',0,100,0)
     
-    forecast_meter_rent_12m=st.sidebar.slider('forecast_meter_rent_12m',0,1000,0)
+    forecast_meter_rent_12m=st.sidebar.slider('forecast_meter_rent_12m',0,200,0)
     
-    forecast_price_energy_p1=st.sidebar.slider('forecast_price_energy_p1',0,1000,0)
+    forecast_price_energy_p1=st.sidebar.slider('forecast_price_energy_p1',0,2,0)
     
-    forecast_price_pow_p1=st.sidebar.slider('forecast_price_pow_p1',0,1000,0)
+    forecast_price_pow_p1=st.sidebar.slider('forecast_price_pow_p1',0,100,0)
     forecast_price_pow_p1=np.log1p(forecast_price_pow_p1)
     
     imp_cons=st.sidebar.slider('imp_cons',0,1000,0)
     imp_cons=np.log1p(imp_cons)
     
-    margin_gross_pow_ele=st.sidebar.slider('margin_gross_pow_ele',-1000,1000,0)
+    margin_gross_pow_ele=st.sidebar.slider('margin_gross_pow_ele',-500,500,0)
     
-    margin_net_pow_ele=st.sidebar.slider('margin_net_pow_ele',-1000,1000,0)
+    margin_net_pow_ele=st.sidebar.slider('margin_net_pow_ele',-700,700,0)
     
-    nb_prod_act=st.sidebar.slider('nb_prod_act',0,1000,0)
+    nb_prod_act=st.sidebar.slider('nb_prod_act',1,40,0)
     
-    net_margin=st.sidebar.slider('net_margin',-1000,1000,0)
+    net_margin=st.sidebar.slider('net_margin',-4000,5000,0)
     
-    num_years_antig=st.sidebar.slider('num_years_antig',0,1000,0)
+    num_years_antig=st.sidebar.slider('num_years_antig',0,20,0)
     
-    pow_max=st.sidebar.slider('pow_max',0,1000,0)
+    pow_max=st.sidebar.slider('pow_max',0,550,0)
     
-    years_end=st.sidebar.slider('years_end',0,1000,0)
+    years_end=st.sidebar.slider('years_end',0,30,0)
     years_end=np.log1p(years_end)
     
-    years_mod=st.sidebar.slider('years_mod',0,1000,0)
+    years_mod=st.sidebar.slider('years_mod',0,30,0)
     
-    tenure=st.sidebar.slider('tenure',0,1000,0)
+    tenure=st.sidebar.slider('tenure',0,50,0)
     
-    total_cons=st.sidebar.slider('total_cons',0,1000,0)
+    total_cons=st.sidebar.slider('total_cons',0,100000,0)
     total_cons=np.log1p(total_cons)
     
-    avg_fct_ene=st.sidebar.slider('avg_fct_ene',0,1000,0)
+    avg_fct_ene=st.sidebar.slider('avg_fct_ene',0,300,0)
     
-    avg_fct_pow=st.sidebar.slider('avg_fct_pow',0,1000,0)
+    avg_fct_pow=st.sidebar.slider('avg_fct_pow',0,300,0)
     
     
     channel_sales=st.sidebar.selectbox('channel_sales',(1,2,3,4,5,6,'Other'))
